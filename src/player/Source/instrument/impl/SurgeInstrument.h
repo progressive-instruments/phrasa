c1fb56f948782ac43d2d4ec88d6d4ae3538359cd
erezblm <erezblumen@gmail.com>
2021-05-26 21:08:35 +0300

    surge_inst: fix note off/on ordering

--- src/player/Source/instrument/impl/SurgeInstrument.h
+++ src/player/Source/instrument/impl/SurgeInstrument.h
@@ -26,7 +26,8 @@ private:
     int m_blockPos;
     double m_sampleTimeMs;
     SequenceProcessor<std::shared_ptr<Event>> m_sequenceProcessor;
-    EventHolder<std::pair<EventType,std::shared_ptr<Event>>> m_eventPool;
+    EventHolder<std::shared_ptr<Event>> m_onEventPool;
+    EventHolder<std::shared_ptr<Event>> m_offEventPool;
 	std::unique_ptr<SurgeSynthesizer> m_surge;
 
 
@@ -44,8 +45,8 @@ private:
 
 	virtual void processBlock(audio::AudioBuffer& buffer, const SequenceTrack& track) override {
         m_sequenceProcessor.consume(track, [this](auto event) {
-            m_eventPool.addEvent(std::make_pair(EventType::ON, event.event), event.relativeTime);
-            m_eventPool.addEvent(std::make_pair(EventType::OFF,event.event), event.relativeTime + event.event->duration);
+            m_onEventPool.addEvent(event.event, event.relativeTime);
+            m_offEventPool.addEvent(event.event, event.relativeTime + event.event->duration);
         });
         
         
@@ -55,18 +56,21 @@ private:
         {
             if (m_blockPos == 0) {
                 SequenceTime time = SequenceTime::FromMilliseconds(m_sampleTimeMs * BLOCK_SIZE);
-                m_eventPool.consume(time, [this](auto event) {
-                if (event.event.second->values.count("frequency")) {
-                    auto midi = getMidiNote(event.event.second->values["frequency"]->getValue());
-                    if (midi >= 0) {
-                        if (event.event.first == EventType::ON) {
-                            m_surge->playNote(1, midi, 127, 0);
-                        }
-                        else {
+                m_offEventPool.consume(time, [this](auto event) {
+                    if (event.event->values.count("frequency")) {
+                        auto midi = getMidiNote(event.event->values["frequency"]->getValue());
+                        if (midi >= 0) {
                             m_surge->releaseNote(1, midi, 127);
                         }
                     }
-                }
+                });
+                m_onEventPool.consume(time, [this](auto event) {
+                    if (event.event->values.count("frequency")) {
+                        auto midi = getMidiNote(event.event->values["frequency"]->getValue());
+                        if (midi >= 0) {
+                            m_surge->playNote(1, midi, 127, 0);
+                        }
+                    }
                 });
                 m_surge->process();
             }
